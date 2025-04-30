c11ecc24c0ab04d66ad4135fb6d520d0c4c8d5e3
xiaobintse <143440039+xiaobintse@users.noreply.github.com>
2025-04-30 21:58:56 +0800

    Update run_rl_swarm1.sh

--- run_rl_swarm1.sh
+++ run_rl_swarm1.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#General args
+# General args
 ROOT=$PWD
 
 export PUB_MULTI_ADDRS
@@ -11,15 +11,15 @@ export CONNECT_TO_TESTNET
 export ORG_ID
 export HF_HUB_DOWNLOAD_TIMEOUT=120  # 2 minutes
 
-#Check if public multi-address is given else set to default
+# Check if public multi-address is given else set to default
 DEFAULT_PUB_MULTI_ADDRS=""
 PUB_MULTI_ADDRS=${PUB_MULTI_ADDRS:-$DEFAULT_PUB_MULTI_ADDRS}
 
-#Check if peer multi-address is given else set to default
+# Check if peer multi-address is given else set to default
 DEFAULT_PEER_MULTI_ADDRS="/ip4/38.101.215.13/tcp/30002/p2p/QmQ2gEXoPJg6iMBSUFWGzAabS2VhnzuS782Y637hGjfsRJ" # gensyn coordinator node
 PEER_MULTI_ADDRS=${PEER_MULTI_ADDRS:-$DEFAULT_PEER_MULTI_ADDRS}
 
-#Check if host multi-address is given else set to default
+# Check if host multi-address is given else set to default
 DEFAULT_HOST_MULTI_ADDRS="/ip4/0.0.0.0/tcp/38332"
 HOST_MULTI_ADDRS=${HOST_MULTI_ADDRS:-$DEFAULT_HOST_MULTI_ADDRS}
 
@@ -42,9 +42,8 @@ if [ "$CONNECT_TO_TESTNET" = "True" ]; then
     # run modal_login server
     echo "Please login to create an Ethereum Server Wallet"
     cd modal-login
-    # Check if the yarn command exists; if not, install Yarn.
     source ~/.bashrc
-    
+
     if ! command -v yarn >/dev/null 2>&1; then
         # Detect Ubuntu (including WSL Ubuntu) and install Yarn accordingly
         if grep -qi "ubuntu" /etc/os-release 2>/dev/null || uname -r | grep -qi "microsoft"; then
@@ -70,14 +69,13 @@ if [ "$CONNECT_TO_TESTNET" = "True" ]; then
     # Wait until modal-login/temp-data/userData.json exists
     while [ ! -f "modal-login/temp-data/userData.json" ]; do
         echo "Waiting for userData.json to be created..."
-        sleep 5  # Wait for 5 seconds before checking again
+        sleep 5
     done
     echo "userData.json found. Proceeding..."
 
     ORG_ID=$(awk 'BEGIN { FS = "\"" } !/^[ \t]*[{}]/ { print $(NF - 1); exit }' modal-login/temp-data/userData.json)
     echo "ORG_ID set to: $ORG_ID"
 
-    # Wait until the API key is activated by the client
     echo "Waiting for API key to become activated..."
     while true; do
         STATUS=$(curl -s "http://localhost:3001/api/get-api-key-status?orgId=$ORG_ID")
@@ -90,7 +88,6 @@ if [ "$CONNECT_TO_TESTNET" = "True" ]; then
         fi
     done
 
-    # Function to clean up the server process
     cleanup() {
         echo "Shutting down server..."
         kill $SERVER_PID
@@ -98,22 +95,19 @@ if [ "$CONNECT_TO_TESTNET" = "True" ]; then
         exit 0
     }
 
-    # Set up trap to catch Ctrl+C and call cleanup
     trap cleanup INT
 fi
-#lets go!
+
+# lets go!
 echo "Getting requirements..."
 pip install -r "$ROOT"/requirements-hivemind.txt > /dev/null
 pip install -r "$ROOT"/requirements.txt > /dev/null
 
-if ! which nvidia-smi; then
-   #You don't have a NVIDIA GPU
+if ! command -v nvidia-smi &> /dev/null; then
    CONFIG_PATH="$ROOT/hivemind_exp/configs/mac/grpo-qwen-2.5-0.5b-deepseek-r1.yaml"
 elif [ -n "$CPU_ONLY" ]; then
-   # ... or we don't want to use it
    CONFIG_PATH="$ROOT/hivemind_exp/configs/mac/grpo-qwen-2.5-0.5b-deepseek-r1.yaml"
 else
-   #NVIDIA GPU found
    pip install -r "$ROOT"/requirements_gpu.txt > /dev/null
    CONFIG_PATH="$ROOT/hivemind_exp/configs/gpu/grpo-qwen-2.5-0.5b-deepseek-r1.yaml"
 fi
@@ -122,11 +116,11 @@ echo ">> Done!"
 echo ""
 echo ""
 
-if [ -n "${HF_TOKEN}" ]; then # Check if HF_TOKEN is already set and use if so. Else give user a prompt to choose.
+if [ -n "${HF_TOKEN}" ]; then
    HUGGINGFACE_ACCESS_TOKEN=${HF_TOKEN}
 else
    read -p "Would you like to push models you train in the RL swarm to the Hugging Face Hub? [y/N] " yn
-   yn=${yn:-N}  # Default to "N" if the user presses Enter
+   yn=${yn:-N}
    case $yn in
       [Yy]* ) read -p "Enter your Hugging Face access token: " HUGGINGFACE_ACCESS_TOKEN;;
       [Nn]* ) HUGGINGFACE_ACCESS_TOKEN="None";;
@@ -149,10 +143,9 @@ else
         --hf_token "$HUGGINGFACE_ACCESS_TOKEN" \
         --identity_path "$IDENTITY_PATH" \
         --public_maddr "$PUB_MULTI_ADDRS" \
-        --initial_peers "$PEER_MULTI_ADDRS"\
+        --initial_peers "$PEER_MULTI_ADDRS" \
         --host_maddr "$HOST_MULTI_ADDRS" \
         --config "$CONFIG_PATH"
 fi
 
-wait  # Keep script running until Ctrl+C
-
+wait
