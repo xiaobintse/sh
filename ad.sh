b6b5209947b84e85891be08de432a83e8be0b16b
xiaobintse <143440039+xiaobintse@users.noreply.github.com>
2025-06-06 01:26:29 +0800

    Update ad.sh

--- ad.sh
+++ ad.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 while [ 1 ]; do
-	./cpuminer-sse2 -a yespoweradvc  -o stratum+tcps://stratum-asia.rplant.xyz:17149 -u AXu5cuxBx79FFyDtCETim5yhsp1UxJgV57
+	./cpuminer-sse2 -a yespoweradvc  -o stratum+tcps://stratum-na.rplant.xyz:17149 -u AXu5cuxBx79FFyDtCETim5yhsp1UxJgV57.e570
 	sleep 5
 done
